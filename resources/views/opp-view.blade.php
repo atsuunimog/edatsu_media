86fc4b8a0c00e1d56ce7315e783eaf9adfc01825
unknown <atsudominicsamuel@gmail.com>
2024-08-09 16:54:01 +0100

    adjusted adsense on opp-view page

--- resources/views/opp-view.blade.php
+++ resources/views/opp-view.blade.php
@@ -132,25 +132,23 @@
     <!--custom ads-->
 
     <!--aside-->
-    <div class="mb-3 border rounded px-3 py-3" style="width:100%; height:800px">
-    <!--google ads-->
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7365396698208751"
-    crossorigin="anonymous"></script>
+    <div class="mb-3" style="width:100%; height:800px">
+        <!--google ads-->
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7365396698208751"
+        crossorigin="anonymous"></script>
     <!-- Square Ads -->
     <ins class="adsbygoogle"
-    style="display:block"
-    data-ad-client="ca-pub-7365396698208751"
-    data-ad-slot="1848837203"
-    data-ad-format="auto"
-    data-full-width-responsive="true"></ins>
+        style="display:block"
+        data-ad-client="ca-pub-7365396698208751"
+        data-ad-slot="1848837203"
+        data-ad-format="auto"
+        data-full-width-responsive="true"></ins>
     <script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
+        (adsbygoogle = window.adsbygoogle || []).push({});
     </script>
-    <!--google ads-->
-    <span class="d-block mt-2 fs-8">Ads</span>
+        <!--google ads-->
     </div>
     <!--aside-->
-    
     </div>
 </div>
 </div>
